40f12e6c0d544bcdbc99b4fb2b8f3a3127f6b276
Marian <hellomariandev@gmail.com>
2022-06-22 23:03:22 +0200

    create and access context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import Booklist from "./components/Booklist";
+import Navbar from "./components/Navbar";
+
+import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <ThemeContextProvider>
+        <Navbar />
+        <Booklist />
+      </ThemeContextProvider>
+    </div>
+  );
 }
 
 export default App;
