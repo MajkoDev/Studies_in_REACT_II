b7f83def00f24386e9ccd96ca2706b8d7abf489b
Marian <hellomariandev@gmail.com>
2022-06-26 00:12:28 +0200

    set up button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import Booklist from "./components/Booklist";
-import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
-
-import AuthContextProvider from "./context/AuthContext";
-import ThemeContextProvider from "./context/ThemeContext";
+import StyledButton from "./components/Button/Button";
 
 function App() {
   return (
     <div className='App'>
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <Booklist />
-          <ThemeToggle />
-        </AuthContextProvider>
-      </ThemeContextProvider>
+      <StyledButton>StyledButton</StyledButton>
     </div>
   );
 }
